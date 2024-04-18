d5e657039a989193da366f4a15ade3fb93d853fc
mavr1ke <mainstip@mail.ru>
2024-04-18 23:05:12 +0200

    update

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const BASE_URL = "http://localhost:8069/api";
-const BASE_URL = "/";
+const BASE_URL = "/api";
 
 const instance = axios.create({
   baseURL: BASE_URL,
