c3f5c2c59f05823b6129f1e39c768d460706ad25
mavr1ke <mainstip@mail.ru>
2024-04-18 23:02:44 +0200

    update

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8069/api";
+// const BASE_URL = "http://localhost:8069/api";
+const BASE_URL = "/";
 
 const instance = axios.create({
   baseURL: BASE_URL,
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // server: {
-  //   open: true,
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:8069",
-  //     },
-  //   },
-  // },
+  server: {
+    open: true,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8069",
+      },
+    },
+  },
 })
